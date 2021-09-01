b53971dc17f96c5d28e847ae73e60aac4db91b1a
zdurr22 <zdurr22@gmail.com>
2021-09-01 12:39:21 -0400

    commit tree 3405ad29b6c10ddd67f78103a84b7a2235ce3677

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+
+  root 'application#home'
 end
